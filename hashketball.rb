7b939b077c35e0d2144c2e7bb05608fba2fa5c1c
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 23:02:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,32 @@
 # Write your code here!
 
+def game_hash
+  game_hash = {
+    :home =>{
+      :team_name => "", #string of the team name
+      :colors =>["","",""], #an array of strings
+      :players =>[
+        {
+          :number => "",
+          :shoe => "",
+          :points => "",
+          :rebounds => "",
+          :assists => "",
+          :steals => "",
+          :blocks => "",
+          
+        },
+      ] #an array of hashes to contain players stats     
+    },
+    :away =>{}
+    
+  }
+end
 
 
 
 
 
 
-
-
+game_hash
 
